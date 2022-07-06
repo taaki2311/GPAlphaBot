0303e42033971c47fd7aae13e7c499fa3f10c63d
taaki2311 <taaki.paul@gmail.com>
2022-07-06 18:33:32 -0400

    just to make sure

---
+++ RaspberryPi/AlphaBotPi.py
@@ -0,0 +1,19 @@
+from picamera import PiCamera
+from socket import *
+
+WIDTH = 128
+HEIGHT = 160
+BUFFER_SIZE = 1024
+HOST = ('192.168.11.229', 55555)
+
+camera = PiCamera(resolution = (WIDTH, HEIGHT))
+camera.rotation = 90
+
+with socket(AF_INET, SOCK_DGRAM) as sock:
+	sock.connect(HOST)
+	for _ in camera.capture_continuous('stream.jpg', 'jpeg'):
+		with open('stream.jpg', 'rb') as stream:
+			data = stream.read(BUFFER_SIZE)
+			while data:
+				sock.sendto(data, HOST)
+				data = stream.read(BUFFER_SIZE)
