de9b899e34844df6bf3178032ac15a5121d3930f
tsingh8 <tsingh8@gmu.edu>
2022-07-06 20:17:19 -0400

    Added try/except block and removed rotation

--- AlphaBotPi/AlphaBotPi.py
+++ AlphaBotPi/AlphaBotPi.py
@@ -7,13 +7,15 @@ BUFFER_SIZE = 1024
 HOST = ('192.168.11.229', 55555)
 
 camera = PiCamera(resolution = (WIDTH, HEIGHT))
-camera.rotation = 90
 
-with socket(AF_INET, SOCK_DGRAM) as sock:
-	while True:
-		camera.capture('stream.jpg', 'jpeg')
-		with open('stream.jpg', 'rb') as stream:
-			data = stream.read(BUFFER_SIZE)
-			while(data):
-				sock.sendto(data, HOST)
-				data = stream.read(BUFFER_SIZE)
+try:
+	with socket(AF_INET, SOCK_DGRAM) as sock:
+			while True:
+				camera.capture('stream.jpg', 'jpeg')
+				with open('stream.jpg', 'rb') as stream:
+					data = stream.read(BUFFER_SIZE)
+					while(data):
+						sock.sendto(data, HOST)
+						data = stream.read(BUFFER_SIZE)
+except KeyboardInterrupt:
+	pass
