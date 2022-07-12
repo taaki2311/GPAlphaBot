3ac1c5f66fe496f742fd008478d105461db93138
taaki2311 <taaki.paul@gmail.com>
2022-07-12 17:12:40 -0400

    Finished with Python

--- RaspberryPi/AlphaBotPi.py
+++ RaspberryPi/AlphaBotPi.py
@@ -1,5 +1,6 @@
 from picamera import PiCamera
 from socket import *
+from io import BytesIO
 
 WIDTH = 128
 HEIGHT = 160
@@ -7,16 +8,16 @@ BUFFER_SIZE = 1024
 HOST = ('192.168.11.229', 55555)
 
 camera = PiCamera(resolution = (WIDTH, HEIGHT))
-
+stream = BytesIO()
 try:
 	with socket(AF_INET, SOCK_DGRAM) as sock:
 		sock.connect(HOST)
-		while True:
-			camera.capture('stream.jpg', 'jpeg')
-			with open('stream.jpg', 'rb') as stream:
+		for _ in camera.capture_continuous(stream, 'jpeg'):
+			stream.seek(0)
+			data = stream.read(BUFFER_SIZE)
+			while data:
+				sock.sendall(data)
 				data = stream.read(BUFFER_SIZE)
-				while(data):
-					sock.send(data)
-					data = stream.read(BUFFER_SIZE)
+			stream.seek(0)
 except KeyboardInterrupt:
 	pass
